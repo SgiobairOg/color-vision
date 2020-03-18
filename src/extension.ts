c5efde32c43edb8fba5c22b1e507424e41699488
Jason Wilson <jason@wilsons.io>
2020-03-18 22:00:27 +0100

    fix(dependencies): Fix High Severity Issue
    Updates https-proxy-agent due to a reported vulnerability

--- src/extension.ts
+++ src/extension.ts
@@ -105,7 +105,7 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
 
 function decorationFactory(
   input: string,
